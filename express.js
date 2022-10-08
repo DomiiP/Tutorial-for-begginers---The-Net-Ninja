0572ddc84b7579cf69ca883853d8bdcb7ee988a9
DomiiP <95188800+DomiiP@users.noreply.github.com>
2022-10-09 00:36:36 +0200

    Learned to pass some parameters and stuff, pretty neat

--- express.js
+++ express.js
@@ -2,6 +2,16 @@
 // Installed express agian (npm install express, you can also npm install express - save)
 var express = require('express'); // Module
 var app = express();
+
+// ---------------------------------------------------
+// - This is from  TUTORIAL #25, but has to be pased here
+
+app.set('view engine', 'ejs'); // Express knows that we use ejs as view engine
+// By default, it is going to look at ./views folder for data
+// ---------------------------------------------------
+
+
+if(false){
 app.get('/',function(req,res){ //function(req,res) is extended in express with additional functionality
     // Express listend to '/' (get request) and when it recieves the request, it sends text down below
     // It is also smart enough to put string into page
@@ -9,8 +19,9 @@ app.get('/',function(req,res){ //function(req,res) is extended in express with a
 }); 
 app.get('/contact',function(req,res){ 
     res.send('this is the contact page');
-}); 
+});  
 app.listen(3000); // Listen to port
+}
 // You can now acces both pages with localhost:3000 or localhost:3000/contact
 
 // HTTP Methods (GET, POST, DELETE, PUT) - can be used with express
@@ -23,3 +34,31 @@ app.listen(3000); // Listen to port
 // Used command: 'Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass'
 
 // ---- End of Video 23 (Introduction to Express)
+
+if(false){
+app.get('/profile/:name',function(req,res){ // We can ID in database (make a database query to return data)
+    res.send('You requested to see a profile with the name of ' + req.params.name); //We can access id with params
+}); }
+
+// ---- End of Video 24 (Express Route Params)
+
+// This is how you load pages easily
+app.get('/',function(req,res){ 
+    res.sendFile(__dirname + '/index.html');
+});
+app.get('/contact',function(req,res){ 
+    res.sendFile(__dirname + '/contact.html');
+})
+
+// Installing ejs with 'npm install ejs - save'
+// ejs - view / template engine to set variables into different templates
+
+
+app.get('/profile/:name',function(req,res){
+    var data = {age: 29, job: 'ninja'};
+    res.render('profile', {person: req.params.name, data: data}); // We render a view by template engines
+    // With parameters we can send data to the template
+}); 
+app.listen(3000);
+
+// ---- End of Video 25 (Template Engines)
